9a3006f8bf8ca4ffdf034163541727c687119140
NicoClack <16543008+hedgehog125@users.noreply.github.com>
2022-06-04 17:58:51 +0100

    Actually, I think resetting the animation might be better

--- src/lib/FancyButton.svelte
+++ src/lib/FancyButton.svelte
@@ -1,262 +1,276 @@
 <script>
-    import {applyClasses} from "../lib/tools.js";
+import { debug } from "svelte/internal";
 
-    /*
-    TODO
+	import {applyClasses} from "../lib/tools.js";
 
-    Is modifying the element directly the best way of doing this? Especially with multiple click effects
+	/*
+	TODO
 
-    Create a default style without a transition so that can be applied with a class. Remove the clicked class, add this new class and then remove it and add the new one back. Or might have to manually change the values without the class applied so no transition
-    Handle multiple touches
-    Fix bug involving shrinking from click making mouse not touch
-    Detect spam click and massively slow down the reverse click animation on release when it's true. Maybe just have multiple tint overlays and keep the button the same size?
-    Alpha effect is too big with distance. Maybe use the square root thing?
-    */
-    
+	Remove spam clicking logic
+	Handle multiple touches
+	Detect spam click and massively slow down the reverse click animation on release when it's true. Maybe just have multiple tint overlays and keep the button the same size?
 
-    const hoverStartTime = 0.25 * 1000;
-    const clickStartTime = 0.15 * 1000;
-    const releaseEarlyDelay = 0.15 * 1000;
-    const reboundTime = 0.15 * 1000;
+	Bugs
+	Involving shrinking from click making mouse not touch
+	Reverse click animation sometimes doesn't play and it disappears instantly. Possibly to do with hover class not applying correctly and no transition without it
+	*/
+	
 
-    const CIRCLE_TO_SQUARE = Math.SQRT1_2;
+	const hoverStartTime = 0.25 * 1000;
+	const clickStartTime = 0.15 * 1000;
+	const releaseEarlyDelay = 0.25 * 1000;
+	const clickEndTime = 0.25 * 1000;
+	const reboundTime = 0.15 * 1000;
 
-    let hoverTask, clickTask, releaseEarlyTask, reboundTask;
-    let hovering = false;
-    let clicking = false;
-    let spamClicking = false;
+	const CIRCLE_TO_SQUARE = Math.SQRT1_2;
 
-    let tintEffects = [
-        {
-            x: 0,
-            y: 0,
-            scale: 0,
-            index: 0
-        }
-    ];
+	let hoverTask, clickTask, reboundTask, clickEndTask;
+	let hovering = false;
+	let clicking = false;
+	let spamClicking = false;
 
-    let display = {
-        hovering: false,
-        clicking: false,
-        rebound: false
-    };
+	let tintEffects = [
+		{
+			x: 0,
+			y: 0,
+			scale: 0,
+			shrinking: false,
+			index: 0
+		}
+	];
+	let display = {
+		hovering: false,
+		clicking: false,
+		rebound: false
+	};
+	let button;
 
-    const endHover = _ => {
-        display.hovering = false;
+	const endHover = _ => {
+		display.hovering = false;
+	};
 
-        tintEffects.splice(1); // Delete all but the last
-        tintEffects = tintEffects;
-    };
+	const enter = _ => {
+		if (reboundTask != null) return;
 
-    const enter = _ => {
-        if (reboundTask != null) return;
+		if (hoverTask == null) clearTimeout(hoverTask);
 
-        if (hoverTask == null) clearTimeout(hoverTask);
+		hoverTask = setTimeout(_ => {
+			if (! hovering) endHover();
 
-        hoverTask = setTimeout(_ => {
-            if (! hovering) endHover();
+			hoverTask = null;
+		}, hoverStartTime);
+		hovering = true;
+		display.hovering = true;
+	};
+	const leave = _ => {
+		if (reboundTask != null) return;
 
-            hoverTask = null;
-        }, hoverStartTime);
-        hovering = true;
-        display.hovering = true;
-    };
-    const leave = _ => {
-        if (reboundTask != null) return;
+		if (hoverTask == null) endHover();
 
-        if (hoverTask == null) endHover();
+		hovering = false;
+	};
 
-        hovering = false;
-    };
+	const endClick = (deleteCount=tintEffects.length) => {
+		if (deleteCount != tintEffects.length) return;
 
-    const endClick = _ => {
-        display.clicking = false;
-        display.rebound = true;
+		console.trace(deleteCount, tintEffects.length);
+		deleteCount = Math.min(deleteCount, tintEffects.length);
 
-        for (let i in tintEffects) {
-            tintEffects[i].scale = 0;
-        }
+		display.clicking = false;
+		display.rebound = true;
 
-        reboundTask = setTimeout(_ => {
-            display.rebound = false;
+		for (let i = 0; i < deleteCount; i++) {
+			tintEffects[i].scale = 0;
+			tintEffects[i].shrinking = true;
+		}
 
-            reboundTask = null;
-        }, reboundTime);
-    };
+		reboundTask = setTimeout(_ => {
+			display.rebound = false;
 
-    const clickStart = e => {
-        if (reboundTask != null) { // Interupt the animation
-            display.rebound = false;
+			reboundTask = null;
+		}, reboundTime);
+		if (deleteCount == tintEffects.length) {
+			clickEndTask = setTimeout(_ => {
+				console.log("A");
+				tintEffects.splice(1); // Delete all but the last
+				tintEffects[0].shrinking = false;
+	
+				clickEndTask = null;
+			}, clickEndTime);
+		}
+	};
 
-            clearTimeout(reboundTask);
-            reboundTask = null;
-        }
-        
+	const clickStart = e => {
+		if (reboundTask != null) { // Interupt the animation
+			display.rebound = false;
 
-        let rect = e.target.getBoundingClientRect();
+			clearTimeout(reboundTask);
+			reboundTask = null;
+		}
 
-        let x = ((e.clientX - rect.left) / rect.width) * 100;
-        let y = ((e.clientY  - rect.top) / rect.height) * 100;
+		let rect = button.getBoundingClientRect();
+		
+		let x = ((e.clientX - rect.left) / rect.width) * 100;
+		let y = ((e.clientY  - rect.top) / rect.height) * 100;
 
-        display.clicking = true;
-        
-        let squareSize = (Math.max(Math.abs(x - 50), Math.abs(y - 50)) / 50) + 1.1;
-        let scale = squareSize / CIRCLE_TO_SQUARE;
+		display.clicking = true;
+		
+		let squareSize = (Math.max(Math.abs(x - 50), Math.abs(y - 50)) / 50) + 1.1;
+		let scale = squareSize / CIRCLE_TO_SQUARE;
 
-        tintEffects[tintEffects.length - 1] = {
-            x: x - 50,
-            y: y - 50,
-            scale: scale,
-            index: 0
-        };
-        //tint.style.left = (x - 50) + "%";
-        //tint.style.top = (y - 50) + "%";
-        //tint.style.transform = `scale(${scale})`;
+		tintEffects[tintEffects.length - 1] = {
+			x: x - 50,
+			y: y - 50,
+			scale: scale,
+			shrinking: false,
+			index: tintEffects.length - 1
+		};
 
+		if (clickTask == null) {
+			clickTask = setTimeout(_ => {
+				clickTask = null;
 
-        if (releaseEarlyTask != null) {
-            endClick();
+				if (! clicking) {
+					(effectCountWas => {
+						setTimeout(_ => {
+							endClick(effectCountWas);
+						}, releaseEarlyDelay);
+					})(tintEffects.length);
+				}
+			}, clickStartTime);
+		}
+		clicking = true;
+	};
+	const clickEnd = _ => {
+		if (reboundTask != null) return;
 
-            clearTimeout(releaseEarlyTask);
-            releaseEarlyTask = null;
-        }
+		tintEffects.push({
+			x: 0,
+			y: 0,
+			scale: 0,
+			shrinking: false,
+			index: tintEffects.length
+		});
+		tintEffects = tintEffects;
+		
+		if (spamClicking) {
+			if (clickTask != null) clearTimeout(clickTask);
 
-        if (clickTask == null) {
-            clickTask = setTimeout(_ => {
-                clickTask = null;
+			clickTask = null;
+		}
+		if (clickTask == null) endClick();
 
-                if (! clicking) {
-                    releaseEarlyTask = setTimeout(_ => {
-                        endClick();
-
-                        releaseEarlyTask = null;
-                    }, releaseEarlyDelay);
-                }
-            }, clickStartTime);
-        }
-        clicking = true;
-    };
-    const clickEnd = _ => {
-        if (reboundTask != null) return;
-
-        if (spamClicking) {
-            if (clickTask) clearTimeout(clickTask);
-            if (releaseEarlyTask) clearTimeout(releaseEarlyTask);
-
-            clickTask = null;
-            releaseEarlyTask = null;
-        }
-        if (clickTask == null && releaseEarlyTask == null) endClick();
-
-        clicking = false;
-    };
+		clicking = false;
+	};
 </script>
 
 <main>
-    <div class="center">
-        <button class={
-            applyClasses(
-                ["large", "hovering", "clicking", "rebound"],
-                [true, display.hovering, display.clicking, display.rebound]
-            )
-        } on:mouseenter={enter} on:mouseleave={leave} on:mousedown={clickStart} on:mouseup={clickEnd}>
-            <div class="content">Hmm</div>
-            {#each tintEffects as tint (tint.index)}
-                <div class="tint" style="left:{tint.x}%;top:{tint.y}%;transform:scale({tint.scale});"></div>
-            {/each}
-        </button>
-    </div>
+	<div class="center">
+		<button class={
+			applyClasses(
+				["large", "hovering", "clicking", "rebound"],
+				[true, display.hovering, display.clicking, display.rebound]
+			)
+		} on:mouseenter={enter} on:mouseleave={leave} on:mousedown={clickStart} on:mouseup={clickEnd} bind:this={button}>
+			<div class="content">Hmm</div>
+			{#each tintEffects as tint (tint.index)}
+				<div class="tint {applyClasses(["shrinking"], [tint.shrinking])}" style="left:{tint.x}%;top:{tint.y}%;transform:scale({tint.scale});">
+				
+				</div>
+			{/each}
+		</button>
+	</div>
 </main>
 
 <style>
-    button {
-        border-radius: 50%;
-        border-width: 3px;
-        border-color: rgba(0, 0, 0, 0);
+	button {
+		border-radius: 50%;
+		border-width: 3px;
+		border-color: rgba(0, 0, 0, 0);
 
-        aspect-ratio: 1;
-        font-weight: bold;
-        background-color: lime;
-        padding: 0px;
+		aspect-ratio: 1;
+		font-weight: bold;
+		background-color: lime;
+		padding: 0px;
 
-        position: relative;
-        overflow: hidden;
-        user-select: none;
+		position: relative;
+		overflow: hidden;
+		user-select: none;
 
-        transition-property: filter, border, transform;
-        transition-duration: 0.25s;
-        transition-timing-function: ease-in-out;
-    }
+		transition-property: filter, border, transform;
+		transition-duration: 0.25s;
+		transition-timing-function: ease-in-out;
+	}
 
-    button > .content {
-        position: relative;
-        top: 0%;
+	button > .content {
+		position: relative;
+		top: 0%;
 
-        padding: 25px;
-    }
+		padding: 25px;
+	}
 
-    button > .tint {
-        position: absolute;
-        top: 0px;
-        left: 50%;
-        width: 100%;
-        height: 100%;
+	button > .tint {
+		position: absolute;
+		top: 0px;
+		left: 50%;
+		width: 100%;
+		height: 100%;
 
-        background-color: rgba(0, 0, 0, 0.1);
-        border-radius: 50%;
+		background-color: rgba(0, 0, 0, 0.15);
+		border-radius: 50%;
 
-        transform: scale(0);
+		transform: scale(0);
 
-        transition-property: transform, background-color;
-    }
+		transition-property: transform, background-color;
+	}
 
-    button.hovering {
-        transform: scale(1.025);
-        border-color: rgba(0, 0, 0, 0.2);
+	button.hovering {
+		transform: scale(1.025);
+		border-color: rgba(0, 0, 0, 0.2);
 
-        transition-duration: 0.25s;
-        filter: brightness(0.94);
-    }
+		transition-duration: 0.25s;
+		filter: brightness(0.94);
+	}
 
-    button.hovering > .tint {
-        transition-duration: 0.25s;
-        transition-timing-function: ease-in;
-    }
+	button.hovering > .tint {
+		transition-duration: 0.25s;
+		transition-timing-function: ease-in;
+	}
 
-    button.clicking {
-        transform: scale(1);
+	button.clicking {
+		transform: scale(1);
 
-        transition-duration: 0.15s;
-        transition-timing-function: ease-in;
-    }
+		transition-duration: 0.15s;
+		transition-timing-function: ease-in;
+	}
 
-    button.clicking > .tint {
-        transform: scale(1);
+	button.clicking > .tint {
+		transform: scale(1);
 
-        transition-duration: 0.15s;
-        transition-timing-function: ease-out;
-    }
+		transition-duration: 0.15s;
+		transition-timing-function: ease-out;
+	}
 
-    button.rebound {
-        transform: scale(1.04);
+	button.rebound {
+		transform: scale(1.04);
 
-        transition-duration: 0.15s;
-        transition-timing-function: ease-out;
-    }
+		transition-duration: 0.15s;
+		transition-timing-function: ease-out;
+	}
 
-    button.rebound > .tint {
-        background-color: rgba(0, 0, 0, 0);
-    }
+	button > .tint.shrinking {
+		background-color: rgba(0, 0, 0, 0);
+	}
 
-    .large {
-        font-size: 100px;
-    }
+	.large {
+		font-size: 100px;
+	}
 
-    .center {
-        position: absolute;
-        left: 50%;
-        top: 50%;
+	.center {
+		position: absolute;
+		left: 50%;
+		top: 50%;
 
-        transform: translate(-50%, -50%);
-    }
+		transform: translate(-50%, -50%);
+	}
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,10 @@
     import FancyButton from "../lib/FancyButton.svelte";
 </script>
 
+<svelte:head>
+    <title>Fancy Buttons Demo</title>
+</svelte:head>
+
 <main>
     <FancyButton></FancyButton>
 </main>
