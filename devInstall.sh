e5b40a3142c40bfad762e162a563f80d90a3f0a5
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-10-29 19:59:34 +0100

    Deleted a bit of the template

--- devInstall.sh
+++ devInstall.sh
@@ -2,19 +2,14 @@ cd $(dirname $0) # The project folder
 echo "Installing...
 "
 
-echo "== 1/3: Installing backend server dependencies ==
-"
-cd server
-npm install
-
 echo "
-== 2/3: Installing static server dependencies ==
+== 1/3: Installing static server dependencies ==
 "
 cd ../static
 npm install
 
 echo "
-== 3/3: Installing gzip tool dependencies ==
+== 2/2: Installing tool dependencies ==
 "
 cd ../tools
 npm install
