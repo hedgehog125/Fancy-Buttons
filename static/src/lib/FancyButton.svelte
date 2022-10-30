d4c17eda65009ef984a3445abd3d82ac2b566016
Nico Clack <16543008+hedgehog125@users.noreply.github.com>
2022-10-30 15:12:15 +0000

    Finished the button component (I think?)
    I think I'm going to work on some page transitions next

--- static/src/lib/FancyButton.svelte
+++ static/src/lib/FancyButton.svelte
@@ -2,7 +2,6 @@
 	/*
 	TODO
 
-	Use extra div and position relative or absolute to prevent layout shift as the button changes size
 	*/
 
 	export let width = 100;
@@ -13,19 +12,22 @@
 	export let onClickStart = null;
 
 	const growAmount = 1.05;
+	const shrinkAmount = 0.95;
 	const minClickEffectStayTime = 150;
 
+	let buttonElement;
 	let hoverEffectElement;
+	let hovering = false;
+
 	let clickEffectElement;
 	let clickEffectX = 0;
 	let clickEffectY = 0;
 	let clickEffectScale = 0;
 	let clickDownEffectFinished = false;
 	let clickEnded = false;
-	let button;	
+	let clicking = false;
 
 	const CIRCLE_TO_SQUARE = Math.SQRT1_2;
-	let hovering = false;
 	const onHover = _ => {
 		hovering = true;
 
@@ -70,7 +72,7 @@
 	const onClickStartInternal = e => {
 		if (onClickStart) onClickStart();
 
-		let rect = button.getBoundingClientRect();
+		let rect = buttonElement.getBoundingClientRect();
 		clickEffectX = ((e.clientX - rect.left) / rect.width) * 100;
 		clickEffectY = ((e.clientY  - rect.top) / rect.height) * 100;
 		let squareSize = (Math.max(Math.abs(clickEffectX - 50), Math.abs(clickEffectY - 50)) * 2) + (growAmount * 100);
@@ -98,6 +100,7 @@
 
 		clickDownEffectFinished = false;
 		clickEnded = false;
+		clicking = true;
 	};
 	const onClickEndInternal = _ => {
 		if (onClick) onClick();
@@ -114,6 +117,8 @@
 		}, minClickEffectStayTime);
 	};
 	const animateClickEffectBack = _ => {
+		clicking = false;
+
 		let animation = clickEffectElement.animate([
 			{
 				width: clickEffectScale,
@@ -135,8 +140,8 @@
 	};
 </script>
 
-<main style="--width:{width}px;--height:{height}px;--color:{color};--growAmount:{growAmount}">
-	<button on:mouseenter={onHover} on:mouseleave={onHoverEnd} on:mousedown={onClickStartInternal} on:mouseup={onClickEndInternal} class:hover={hovering} bind:this={button}>
+<main style="--width:{width}px;--height:{height}px;--color:{color};--growAmount:{growAmount};--shrinkAmount:{shrinkAmount}">
+	<button on:mouseenter={onHover} on:mouseleave={onHoverEnd} on:mousedown={onClickStartInternal} on:mouseup={onClickEndInternal} class:hover={hovering} class:click={clicking} bind:this={buttonElement}>
 		<div class="hover effect" bind:this={hoverEffectElement}></div>
 		<div class="click effect" bind:this={clickEffectElement} style="top:{clickEffectY}%;left:{clickEffectX}%"></div>
 		<div class="content">
@@ -146,24 +151,46 @@
 </main>
 
 <style>
-	button {
+	* {
+		user-select: none;
+	}
+
+	main {
 		position: relative;
+		width: var(--width);
+		height: var(--height);
+	}
+
+	button {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
 
 		width: var(--width);
 		height: var(--height);
 		background-color: var(--color);
-		border: 2px solid rgba(0, 0, 0, 0.15);
+
+		border-width: 2px;
+		border-style: solid;
+		border-color: rgba(0, 0, 0, 0.15);
+
 		border-radius: calc(var(--height) / 2);
-		transition-property: width, height, border-radius;
+		transition-property: width, height, border-radius, border-width;
 		transition-duration: 0.25s;
 
 		overflow: hidden;
-		user-select: none;
 	}
-	button.hover {
+	button.hover:not(.click) {
 		width: calc(var(--width) * var(--growAmount));
 		height: calc(var(--height) * var(--growAmount));
 		border-radius: calc((var(--height) * var(--growAmount)) / 2);
+	}
+	button.click {
+		width: calc(var(--width) * var(--shrinkAmount));
+		height: calc(var(--height) * var(--shrinkAmount));
+		border-radius: calc((var(--height) * var(--shrinkAmount)) / 2);
+		border-width: 1.5px;
 	}
 
 	.effect, .content {
@@ -179,7 +206,7 @@
 		border-radius: 50%;
 	}
 	.hover.effect {
-		background-color: rgba(255, 255, 255, 0.1);
+		background-color: rgba(255, 255, 255, 0.075);
 	}
 	.click.effect {
 		background-color: rgba(0, 0, 0, 0.1);
@@ -192,7 +219,10 @@
 		transition-property: letter-spacing;
 		transition-duration: 0.25s;
 	}
-	button.hover > .content {
+	button.hover:not(.click) > .content {
 		letter-spacing: 1px;
+	}
+	button.click > .content {
+		letter-spacing: -0.5px;
 	}
 </style>
